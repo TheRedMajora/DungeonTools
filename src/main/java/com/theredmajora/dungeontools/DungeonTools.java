aadc7a7ea883de8c68c35ac69a0aebc92e511ded
TheRedMajora <theredmajora@gmail.com>
2019-03-22 01:18:17 +0100

    2.1

--- src/main/java/com/theredmajora/dungeontools/DungeonTools.java
+++ src/main/java/com/theredmajora/dungeontools/DungeonTools.java
@@ -36,7 +36,7 @@ public class DungeonTools
 	public static DungeonTools instance = new DungeonTools();
 	
     public static final String ID = "dungeontools";
-    public static final String VERSION = "2.0";
+    public static final String VERSION = "2.1";
     
     public static CreativeTabs dungeonTab = new CreativeTabs("dungeonTab")
     {
